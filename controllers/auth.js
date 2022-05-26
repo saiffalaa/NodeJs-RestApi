94d56e7ae2ff1a5a71fd75cdcc0da49268e58486
Seif Alaa Seif El Nasr <SeSeifElnasr@aaib.com>
2022-05-26 17:41:04 +0200

    testing

--- controllers/auth.js
+++ controllers/auth.js
@@ -56,11 +56,49 @@ exports.login = (req, res, next) => {
         { expiresIn: "1h" }
       );
       res.status(200).json({ token, userId: loadedUser._id.toString() });
+      return;
     })
     .catch((err) => {
       if (!err.statusCode) {
         err.statusCode = 500;
       }
       next(err);
+      return err;
     });
+};
+exports.getUserStatus = async (req, res, next) => {
+  try {
+    const user = await User.findById(req.userId);
+    if (!user) {
+      const error = new Error("User not found.");
+      error.statusCode = 404;
+      throw error;
+    }
+    res.status(200).json({ status: user.status });
+  } catch (err) {
+    if (!err.statusCode) {
+      err.statusCode = 500;
+    }
+    next(err);
+  }
+};
+
+exports.updateUserStatus = async (req, res, next) => {
+  const newStatus = req.body.status;
+  try {
+    const user = await User.findById(req.userId);
+    if (!user) {
+      const error = new Error("User not found.");
+      error.statusCode = 404;
+      throw error;
+    }
+    user.status = newStatus;
+    await user.save();
+    res.status(200).json({ message: "User updated." });
+  } catch (err) {
+    if (!err.statusCode) {
+      err.statusCode = 500;
+    }
+    next(err);
+  }
 };
---
+++ test/start.js
@@ -0,0 +1,6 @@
+// const expect = require("chai").expect;
+// it("should add numbers correctly", function () {
+//   const n = 1;
+//   const n2 = 3;
+//   expect(n+n2).to.equal(4)
+// });
